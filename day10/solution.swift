75efc33db54a950cc47c1974d8d1da2f40a5e1b2
Jim Boulter <jboulter11@gmail.com>
2022-12-10 17:26:11 -0800

    day 10, but maybe has a bug?

--- day10/solution.swift
+++ day10/solution.swift
@@ -2,56 +2,75 @@
 import Foundation
 
 let rawInput = try! String(contentsOf: URL(filePath: "./input.txt"), encoding: .utf8)
-let lines = rawInput.split(separator: "\n")
+var lines = rawInput.split(separator: "\n")
 
-struct Point: Hashable {
-	var x: Int
-	var y: Int
+
+let readCycles = [20, 60, 100, 140, 180, 220]
+var readValues = [Int]()
+func isReadCycle(n: Int) -> Bool {
+	return (n - 20) % 40 == 0
 }
-var tailLocations = Set<Point>()
 
-var rope = Array(repeating: Point(x: 0, y: 0), count: 10)// change count to 2 for part 1
-
-for line in lines {
-	let cmd = line.split(separator: " ")
-	let direction = cmd[0]
-	let count = Int(cmd[1])!
+var instruction: (cmd: String, arg: Int)?
+var clock = 1
+var reg = 1
+var wait = 0
+while lines.count > 0 || instruction != nil {
+//	print("beginning clock cycle \(clock)")
+	if instruction == nil {
+		instruction = parseNextInstruction()
+		prep(instruction: instruction!)
+	}
 	
-	for _ in 0..<count {
-		switch direction {
-			case "L":
-				rope[0].x -= 1
-			case "R":
-				rope[0].x += 1
-			case "U":
-				rope[0].y += 1
-			case "D":
-				rope[0].y -= 1
-			default:
-				fatalError("no")
-		}
-		
-		for t in 1..<rope.count {
-			let h = t-1 // head index = tail index - 1
-			let xDist = rope[h].x - rope[t].x
-			let yDist = rope[h].y - rope[t].y
-			
-			// not touching, not cardinal
-			if abs(xDist) + abs(yDist) > 2 {
-				// diagonal
-				rope[t].x += xDist/abs(xDist)
-				rope[t].y += yDist/abs(yDist)
-			} else {
-				// cardinal moves
-				rope[t].x += xDist/2
-				rope[t].y += yDist/2
-			}
-		}
-		
-		// log tail location
-		tailLocations.insert(rope[rope.count-1])
+	// reading during cycle
+	renderPixel()
+	if readCycles.contains(clock) {
+//		print("reading \(clock) * \(reg) == \(clock*reg)")
+		readValues.append(clock * reg)
+	}
+	
+//	print("cycle \(clock) ends")
+	wait -= 1
+	clock += 1
+	if wait == 0 {
+//		print("completing instruction \(instruction!)")
+		complete(instruction: instruction!)
+		instruction = nil
+	}
+	
+}
+
+func parseNextInstruction() -> (cmd: String, arg: Int) {
+	let line = lines.removeFirst().split(separator: " ")
+	return (String(line[0]), line.count == 2 ? Int(String(line[1]))! : 0)
+}
+
+func prep(instruction: (cmd: String, arg: Int)) {
+	if instruction.cmd == "addx" {
+		wait = 2
+	} else {
+		wait = 1
 	}
 }
 
-let partOne = tailLocations.count
-print("Part one or two: \(partOne)")
+func renderPixel() {
+	let charToPrint: String
+	let modifiedClock = clock % 40
+	if abs(modifiedClock - reg) <= 2 && modifiedClock - reg >= 0 {
+		charToPrint = "#"
+	} else {
+		charToPrint = "."
+	}
+	print(charToPrint, terminator: modifiedClock == 0  ? "\n" : "")
+}
+
+func complete(instruction: (cmd: String, arg: Int)) {
+	if instruction.cmd == "addx" {
+		reg += instruction.arg
+	} else {
+		// no op
+	}
+}
+
+let partOne = readValues.reduce(0, +)
+print("Part one: \(partOne)")
